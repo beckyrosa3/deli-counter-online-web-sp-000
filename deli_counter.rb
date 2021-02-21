85e2d51a4476a74c0780f46a4fc4b4b25c406b01
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-21 19:14:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+    if katz_deli.size == 0
+      puts "The line is currently empty."
+    else
+      name = []
+      katz_deli.each_with_index do |name,index|
+      name.join(' ') << "The line is currently: #{index + 1}. #{name}"}
+    end
+  end
+end
+#def take_a_number(katz_deli, name)
+#  if katz_deli.size == 0
+#  puts katz_deli >> name
+#  puts "Welcome #{name}.  You are number #{katz_deli+1} in line."
+#end
+#end
